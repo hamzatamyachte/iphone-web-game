78a0debe51bd7fa9c04a4f86b272ce4db6df848f
Hamza Tamyachte <hamza.tamyachte@gmail.com>
2026-09-25 16:21:30 +0200

    Add SVG animals, coins, animal shop, main menu and pause

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump VERSION after changing any file so installed copies pick up the update.
-const VERSION = 'dodge-v2';
+const VERSION = 'dodge-v3';
 const FILES = ['./', 'index.html', 'manifest.json', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
